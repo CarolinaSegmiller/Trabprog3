263bbcb22a770eea98a1d4a605b4f716f3656248
Carolina Segmiller <66015838+CarolinaSegmiller@users.noreply.github.com>
2024-04-18 10:50:50 -0300

    segundo commit

--- src/controle/Programa.java
+++ src/controle/Programa.java
@@ -1,11 +1,48 @@
-package controle;
+import java.util.ArrayList;
 
-import view.Cadastro;
+import dominio.Fase;
 
-public class Programa {
-	public static void main(String[] args) {
-		Cadastro cadastro = new Cadastro();
-		
-		cadastro.setVisible(true);
-	}
+// Classe representando um personagem
+class Personagem {
+    private String nome;
+    
+    public Personagem(String nome) {
+        this.nome = nome;
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+}
+
+// Classe representando uma fase
+class Fase {
+    private String nome;
+    
+    public Fase(String nome) {
+        this.nome = nome;
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+}
+
+// Classe representando um mundo
+class Mundo {
+    private String nome;
+    private ArrayList<Fase> fases;
+    
+    public Mundo(String nome) {
+        this.nome = nome;
+        fases = new ArrayList<>();
+    }
+    
+    public void addFase(Fase fase) {
+        fases.add(fase);
+    }
+    
+    public ArrayList<Fase> getFases() {
+        return fases;
+    }
 }
