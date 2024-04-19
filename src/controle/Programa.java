4274e5f2bf33079177176567915d82fce9e70844
Carolina Segmiller <66015838+CarolinaSegmiller@users.noreply.github.com>
2024-04-19 14:34:31 -0300

    setimo commit

--- src/controle/Programa.java
+++ src/controle/Programa.java
@@ -1,51 +1,11 @@
-import java.util.ArrayList;
+package controle;
 
-// Classe representando um personagem do sistema solar
-class Personagem {
-    private String nome;
-    
-    public Personagem(String nome) {
-        this.nome = nome;
-    }
-    
-    public String getNome() {
-        return nome;
-    }
-}
+import view.Mapa;
 
-// Classe representando uma fase do sistema solar
-class Satelite {
-    private String nome;
-    
-    public Satelite(String nome) {
-        this.nome = nome;
-    }
-    
-    public String getNome() {
-        return nome;
-    }
-}
+public class Programa {
+    public static void main(String[] args) {
+        Mapa mapa = new Mapa();
 
-// Classe representando um mundo do sistema solar
-class Planeta {
-    private String nome;
-    private ArrayList<Satelite> satelites;
-    
-    public Planeta(String nome) {
-        this.nome = nome;
-        satelites = new ArrayList<>();
-    }
-    
-    public void addSatelite(Satelite satelite) {
-        satelites.add(satelite);
-    }
-    
-    public ArrayList<Satelite> getSatelites() {
-        return satelites;
-    }
-}
-    
-    public ArrayList<Fase> getFases() {
-        return fases;
+        mapa.setVisible(true);
     }
 }
